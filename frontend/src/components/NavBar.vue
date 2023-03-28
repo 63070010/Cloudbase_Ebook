d6d3674a339c914fff59fa0299ebcd369177ade8
it63070179 <63070179@kmitl.ac.th>
2023-03-28 21:38:59 +0700

    test แก้ใหม่

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar is-fixed-top" style="background-color: red">
+    <nav class="navbar is-fixed-top" style="background-color: pink">
       <div class="navbar-menu mt-1 mb-1">
         <div class="navbar-start ml-3">
           <div v-if="!user" class="navbar-item">
