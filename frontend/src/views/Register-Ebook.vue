08c2eece9e38bc404ad56761c61cdffed8e296d8
it63070179 <63070179@kmitl.ac.th>
2023-04-02 23:21:21 +0700

    Change Register

--- frontend/src/views/Register-Ebook.vue
+++ frontend/src/views/Register-Ebook.vue
@@ -28,11 +28,19 @@
               <input
                 class="input is-medium is-rounded"
                 type="text"
+                placeholder="กรอกชื่อผู้ใช้"
                 style="background-color: #eee2dc"
-                v-model="username"
+                v-model="state.username"
               />
               <span class="icon is-small is-left">
                 <i class="fas fa-user"></i>
+              </span>
+              <span
+                v-for="error in v$.username.$errors"
+                :key="error.$uid"
+                class="has-text-danger"
+              >
+                {{ error.$message }}
               </span>
             </div>
           </div>
@@ -43,23 +51,44 @@
               <input
                 class="input is-medium is-rounded"
                 type="text"
-                v-model="password"
+                placeholder="กรอกรหัสผ่าน"
+                v-model="state.password"
                 style="background-color: #eee2dc"
               />
               <span class="icon is-small is-left">
                 <i class="fas fa-key"></i>
+              </span>
+              <span
+                v-for="error in v$.password.$errors"
+                :key="error.$uid"
+                class="has-text-danger"
+              >
+                {{ error.$message }}
               </span>
             </div>
           </div>
 
           <div class="field">
             <label class="label" style="color: #ac3b61">ยืนยัน รหัสผ่าน</label>
-            <input
-              class="input is-medium is-rounded"
-              type="text"
-              v-model="confirm_password"
-              style="background-color: #eee2dc"
-            />
+            <div class="control has-icons-left has-icons-right">
+              <input
+                class="input is-medium is-rounded"
+                type="text"
+                placeholder="กรอกรหัสผ่านอีกครั้ง"
+                v-model="state.confirm_password"
+                style="background-color: #eee2dc"
+              />
+              <span class="icon is-small is-left">
+                <i class="fas fa-key"></i>
+              </span>
+              <span
+                v-for="error in v$.confirm_password.$errors"
+                :key="error.$uid"
+                class="has-text-danger"
+              >
+                {{ error.$message }}
+              </span>
+            </div>
           </div>
 
           <div class="field">
@@ -68,30 +97,44 @@
               <input
                 class="input is-medium is-rounded"
                 type="email"
-                v-model="email"
+                placeholder="กรอกอีเมล"
+                v-model="state.email"
                 style="background-color: #eee2dc"
               />
               <span class="icon is-small is-left">
                 <i class="fas fa-envelope"></i>
+              </span>
+              <span
+                v-for="error in v$.email.$errors"
+                :key="error.$uid"
+                class="has-text-danger"
+              >
+                {{ error.$message }}
               </span>
             </div>
           </div>
 
           <div class="field-body">
             <div class="field">
-                <label class="label" style="color: #ac3b61"
-                  >เบอร์โทรศัพท์</label
-                >
-                <div class="control has-icons-left has-icons-right">
+              <label class="label" style="color: #ac3b61">เบอร์โทรศัพท์</label>
+              <div class="control has-icons-left has-icons-right">
                 <input
                   :disabled="disabled"
                   class="input is-medium is-rounded"
                   type="text"
-                  v-model="mobile"
+                  placeholder="กรอกเบอร์โทรศัพท์"
+                  v-model="state.mobile"
                   style="background-color: #eee2dc"
                 />
                 <span class="icon is-small is-left">
                   <i class="fas fa-phone"></i>
+                </span>
+                <span
+                  v-for="error in v$.mobile.$errors"
+                  :key="error.$uid"
+                  class="has-text-danger"
+                >
+                  {{ error.$message }}
                 </span>
               </div>
             </div>
@@ -100,20 +143,28 @@
               <label class="label" style="color: #ac3b61">เพศ</label>
 
               <div class="select is-fullwidth is-medium is-rounded">
-                <select v-model="sex" style="background-color: #eee2dc">
+                <select v-model="state.sex" style="background-color: #eee2dc">
                   <option value="Male">ชาย</option>
                   <option value="Famale">หญิง</option>
                   <option value="not_specified">ไม่ระบุ</option>
                 </select>
               </div>
+
+              <span
+                v-for="error in v$.sex.$errors"
+                :key="error.$uid"
+                class="has-text-danger"
+              >
+                {{ error.$message }}
+              </span>
             </div>
           </div>
           <div class="field column">
             <div class="control has-text-centered">
               <button
                 class="button is-rounded is-medium"
-                style="color: #eee2dc; background-color: #ac3b61;"
-                @click="submit()"
+                style="color: #eee2dc; background-color: #ac3b61"
+                @click="submit"
               >
                 สมัครสมาชิก
               </button>
@@ -125,19 +176,56 @@
   </div>
 </template>
 <script>
+import useValidate from "@vuelidate/core";
+import {
+  required,
+  email,
+  minLength,
+  maxLength,
+  sameAs,
+} from "@vuelidate/validators";
+import { reactive, computed } from "vue";
 export default {
-  data() {
-    return {
+  setup() {
+    const state = reactive({
       username: "",
       password: "",
       confirm_password: "",
       email: "",
       mobile: "",
-      first_name: "",
-      last_name: "",
       sex: "",
-      birthday: "",
+    });
+    const rules = computed(() => {
+      return {
+        username: { required, minLength: minLength(4) },
+        password: { required, minLength: minLength(6) },
+        confirm_password: { required, sameAs: sameAs(state.password) },
+        email: { required, email },
+        mobile: {
+          required,
+          minLength: minLength(10),
+          maxLength: maxLength(10),
+        },
+        sex: { required },
+      };
+    });
+
+    const v$ = useValidate(rules, state);
+
+    return {
+      state,
+      v$,
     };
+  },
+  methods: {
+    submit() {
+      this.v$.$validate();
+      if (this.v$.$error) {
+        alert("Form failed validation");
+      } else {
+        alert("Form successfully submitted.");
+      }
+    },
   },
 };
 </script>
