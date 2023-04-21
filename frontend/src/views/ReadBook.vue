4f2c9c8d8f9706f0dbbec684d988e96e44d41fda
it63070179 <63070179@kmitl.ac.th>
2023-04-21 18:34:44 +0700

    Change

--- frontend/src/views/ReadBook.vue
+++ frontend/src/views/ReadBook.vue
@@ -2,7 +2,9 @@
   <div>
     <NavBar />
     <br />
-    <div id="webviewer" ref="viewer"></div>
+    <div id="webviewer" ref="viewer">
+      <div class="rectangle" v-if="isLoaded == true"></div>
+    </div>
   </div>
 </template>
 
@@ -16,21 +18,34 @@ export default {
   components: {
     NavBar,
   },
+  data() {
+    return {
+      isLoaded: false,
+    };
+  },
   setup() {
     const viewer = ref(null);
+
     onMounted(() => {
       const path = `https://firebasestorage.googleapis.com/v0/b/image-a4852.appspot.com/o/cloud-midterm.pdf?alt=media&token=22d57546-6295-4300-bbdb-7513479e0df4&v`;
-      WebViewer(
-        {
-          path,
-        },
-        viewer.value
-      );
+      setTimeout(() => {
+        WebViewer(
+          {
+            path,
+          },
+          viewer.value
+        );
+      }, 1000);
     });
 
     return {
       viewer,
     };
+  },
+  created() {
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 1800);
   },
 };
 </script>
@@ -38,5 +53,15 @@ export default {
 <style>
 #webviewer {
   height: 100vh;
+  position: relative;
+}
+.rectangle {
+  width: 120px;
+  height: 40px;
+  background-color: #333333;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 2;
 }
 </style>
