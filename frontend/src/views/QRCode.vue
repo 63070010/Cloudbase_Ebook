f4767b22542608d1b951293d3d026e9c069276f4
it63070179 <63070179@kmitl.ac.th>
2023-04-23 13:09:58 +0700

    UPDATE

--- frontend/src/views/QRCode.vue
+++ frontend/src/views/QRCode.vue
@@ -1,26 +1,111 @@
 <template>
   <div>
-    <vue-qrcode :value="test()"></vue-qrcode>
+    <div class="qrcode-container">
+      <div class="qrcode-wrapper">
+        <canvas ref="canvas"></canvas>
+        <span class="qrcode-text">สแกน QR Code เพื่อชำระเงิน</span><br />
+        <span class="qrcode-text2">ยอดชำระ {{ totalprice }} บาท </span>
+      </div>
+      <router-link to="/" style="margin-bottom: -600px; position: absolute"
+        ><button
+          class="button is-info is-rounded"
+        >
+          กลับไปหน้าแรก
+        </button></router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
-import VueQrcode from 'vue-qrcode';
+import QRCode from "qrcode";
+import axios from "axios";
 
 export default {
-  name: 'QrCodeWithCheck',
-  components: {
-    VueQrcode,
-  },
+  name: "QrCodeWithCheck",
   data() {
     return {
+      qrCodeValue: "http://localhost:8080/",
+      id: 1,
+      cart: [],
+      totalprice: 0,
     };
   },
+  created() {
+    this.getcartitem();
+  },
+  mounted() {
+    const canvas = this.$refs.canvas;
+    QRCode.toCanvas(canvas, this.qrCodeValue, this.qrCodeOptions, (error) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("QR Code is generated!");
+      }
+    });
+    canvas.style.width = "300px";
+    canvas.style.height = "300px";
+  },
   methods: {
-    test() {
-      console.log(1);
-      return "https://fengyuanchen.github.io/vue-qrcode";
+    async getcartitem() {
+      try {
+        axios
+          .get(
+            `https://5ixfubta0m.execute-api.us-east-1.amazonaws.com/ebook/cart?id=${this.id}`
+          )
+          .then((response) => {
+            this.cart = response.data;
+            this.totalprice = this.cart[0].price;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
 </script>
+
+<style>
+.qrcode-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f2f2f2;
+}
+
+.qrcode-wrapper {
+  width: 80vw;
+  max-width: 400px;
+  height: 80vw;
+  max-height: 400px;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.qrcode-text {
+  position: absolute;
+  margin-bottom: -300px;
+  left: 0;
+  width: 100%;
+  text-align: center;
+}
+.qrcode-text2 {
+  position: absolute;
+  margin-bottom: -350px;
+  left: 0;
+  width: 100%;
+  text-align: center;
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+}
+</style>
